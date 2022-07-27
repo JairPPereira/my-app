783807012960331187405e6b3c5e0852d66f8bf0
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-27 20:54:05 -0300

    Update Umpobretaoncsb.js

--- src/components/pages/Umpobretaoncsb.js
+++ src/components/pages/Umpobretaoncsb.js
@@ -3,55 +3,55 @@ import VideoPlayer from 'react-video-js-player';
 import { Link } from 'react-router-dom';
 import './videoapp.css';
 
- 
+
 class Umpobretaoncsb extends Component {
     player = {}
     state = {
         video: {
-            src: "https://www.netmovies.com.br/818d83d6-1589-44bc-8db8-cf44532ce8fa",
+            src: "https://play.prod.gcp.vix.services/crossing-point_pt/crossing-point_pt_1-1/play_v1_hls_234p.m3u8",
 
             poster: ""
         }
     }
- 
-    onPlayerReady(player){
+
+    onPlayerReady(player) {
         console.log("Player is ready: ", player);
         this.player = player;
     }
- 
-    onVideoPlay(duration){
+
+    onVideoPlay(duration) {
         console.log("Video played at: ", duration);
     }
- 
-    onVideoPause(duration){
+
+    onVideoPause(duration) {
         console.log("Video paused at: ", duration);
     }
- 
-    onVideoTimeUpdate(duration){
+
+    onVideoTimeUpdate(duration) {
         console.log("Time updated: ", duration);
     }
- 
-    onVideoSeeking(duration){
+
+    onVideoSeeking(duration) {
         console.log("Video seeking: ", duration);
     }
- 
-    onVideoSeeked(from, to){
+
+    onVideoSeeked(from, to) {
         console.log(`Video seeked from ${from} to ${to}`);
     }
- 
-    onVideoEnd(){
+
+    onVideoEnd() {
         console.log("Video ended");
     }
- 
+
     render() {
         return (
             <div>
-                      <p><br /></p>
+                <p><br /></p>
                 <VideoPlayer
                     controls={true}
                     src={this.state.video.src}
                     poster={this.state.video.poster}
-                    width="720px" 
+                    width="720px"
                     height="auto"
                     onReady={this.onPlayerReady.bind(this)}
                     onPlay={this.onVideoPlay.bind(this)}
@@ -61,16 +61,16 @@ class Umpobretaoncsb extends Component {
                     onSeeked={this.onVideoSeeked.bind(this)}
                     onEnd={this.onVideoEnd.bind(this)}
                 />
-                    <p><br /></p> 
-               <div className="nave">
-      <nav>
-      <p><br /></p>
-      <Link to="/">Home</Link>
+                <p><br /></p>
+                <div className="nave">
+                    <nav>
+                        <p><br /></p>
+                        <Link to="/">Home</Link>
 
-<p><br /></p>
-<p>teste vix</p>
-      </nav></div></div>
-            
+                        <p><br /></p>
+                        <p>teste vix</p>
+                    </nav></div></div>
+
         );
     }
 }
