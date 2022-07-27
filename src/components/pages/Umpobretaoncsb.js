e5023990ff1fa499e59a2cc9c904563691a29fdf
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-27 08:21:52 -0300

    Update Umpobretaoncsb.js

--- src/components/pages/Umpobretaoncsb.js
+++ src/components/pages/Umpobretaoncsb.js
@@ -1,31 +1,77 @@
-import React from "react";
+import React, { Component } from 'react';
+import VideoPlayer from 'react-video-js-player';
 import { Link } from 'react-router-dom';
 import './videoapp.css';
 
-export default function Sbt() {
-  return (
-    <div>
-          <p><br /></p>
-      <iframe
-        src="https://ottvsmediaservice.ottvs.com.br/57501e29-9e90-4bf1-8784-645b95b40035/0151359_movie.ism/manifest(format=mpd-time-csf,encryption=cenc)"
-        frameborder="0"
-        width="100%" height="320px"
-        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
-        title="video"
-      />{" "}
-                  <div className="nave">
+ 
+class Umpobretaoncsb extends Component {
+    player = {}
+    state = {
+        video: {
+            src: "https://play.prod.gcp.vix.services/crossing-point_pt/crossing-point_pt_1-1/play_v1_hls_234p.m3u8",
+
+            poster: ""
+        }
+    }
+ 
+    onPlayerReady(player){
+        console.log("Player is ready: ", player);
+        this.player = player;
+    }
+ 
+    onVideoPlay(duration){
+        console.log("Video played at: ", duration);
+    }
+ 
+    onVideoPause(duration){
+        console.log("Video paused at: ", duration);
+    }
+ 
+    onVideoTimeUpdate(duration){
+        console.log("Time updated: ", duration);
+    }
+ 
+    onVideoSeeking(duration){
+        console.log("Video seeking: ", duration);
+    }
+ 
+    onVideoSeeked(from, to){
+        console.log(`Video seeked from ${from} to ${to}`);
+    }
+ 
+    onVideoEnd(){
+        console.log("Video ended");
+    }
+ 
+    render() {
+        return (
+            <div>
+                      <p><br /></p>
+                <VideoPlayer
+                    controls={true}
+                    src={this.state.video.src}
+                    poster={this.state.video.poster}
+                    width="720px" 
+                    height="auto"
+                    onReady={this.onPlayerReady.bind(this)}
+                    onPlay={this.onVideoPlay.bind(this)}
+                    onPause={this.onVideoPause.bind(this)}
+                    onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
+                    onSeeking={this.onVideoSeeking.bind(this)}
+                    onSeeked={this.onVideoSeeked.bind(this)}
+                    onEnd={this.onVideoEnd.bind(this)}
+                />
+                    <p><br /></p> 
+               <div className="nave">
       <nav>
       <p><br /></p>
       <Link to="/">Home</Link>
 
 <p><br /></p>
-
+<p>teste vix</p>
       </nav></div></div>
-
-  );
+            
+        );
+    }
 }
-
-
-
-
-
+export default Umpobretaoncsb;
